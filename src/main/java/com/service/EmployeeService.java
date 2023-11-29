e4aea41f5e1ca3d454b2edb9f5eacd06abb68c02
shraddhamandhane <mandhaneshraddha123@gmail.com>
2023-11-29 07:31:32 -0600

    this is committed

---
+++ src/main/java/com/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.service;
+
+import java.util.List;
+
+import com.entity.EmployeeEntity;
+
+public interface EmployeeService {
+
+	void saveEmployee(EmployeeEntity employeeEntity);
+
+	List<EmployeeEntity> findAllEmployees();
+
+	EmployeeEntity authenticate(String email, String password);
+
+	void deleteEmp(int employeeId);
+
+	
+	
+
+	
+
+}
